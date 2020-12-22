95717a5b436a7dabf9ce303e4220d925ae372fc5
Hope <full.of.hope@hotmail.com>
2020-12-22 13:44:07 -0500

    committing all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import Header from './components/Header'
+import { BrowserRouter} from 'react-router-dom';
+import Routes from './components/routes';
 import './App.css';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
